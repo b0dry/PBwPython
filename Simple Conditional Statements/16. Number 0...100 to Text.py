fa559f18d7f45a0f974d9d0b336d6327df214215
dobromirdinkov <dobrydinkov@yahoo.com>
2018-04-28 14:58:28 +0300

    New Exercise added

---
+++ Simple Conditional Statements/16. Number 0...100 to Text.py
@@ -0,0 +1,18 @@
+# https://judge.softuni.bg/Contests/Practice/Index/152#15
+
+a = int(input())
+twenties = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
+            'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen',
+            'nineteen']
+bigger = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety', 'one hundred']
+result = 'invalid number'
+
+if 0 <= a <= 100:
+    if a > 19:
+        result = bigger[a // 10 - 2]
+        if a % 10 != 0:
+            result = result + ' ' + twenties[a % 10]
+    else:
+        result = twenties[a]
+
+print(result)
