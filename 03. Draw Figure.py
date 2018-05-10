1b19eb4028e16715c175686e701880b6223bbab4
b0dry <dobrydinkov@yahoo.com>
2018-05-10 18:10:49 +0300

    Additional Exercises

--- 03. Draw Figure.py
+++ 03. Draw Figure.py
@@ -1,11 +1,13 @@
 # https://judge.softuni.bg/Contests/Practice/Index/1#2
 
 number = int(input())
-for column in range(0, number):
-    print(row * '.', end='')
-    for row in range(0, number):
-        print((abs(row - column) // 2) * '*', end='')
-        print('.', end='')
-        row += 1
-    print('')
-    column += 1
+
+for column in range(number // 2 + 1):
+    d = column * '.'
+    s = (number - 2 * column) * '*'
+    print(f'{d}{s}{d}')
+
+for c in range(number // 2 - 1, -1, -1):
+    d = c * '.'
+    s = (number - 2 * c) * '*'
+    print(f'{d}{s}{d}')
