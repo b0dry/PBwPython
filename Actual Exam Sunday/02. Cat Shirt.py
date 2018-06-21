69a1ad6ee775dc1884727c7617f45afa981e42c3
b0dry <dobrydinkov@yahoo.com>
2018-06-21 16:42:53 +0300

    Small refactoring

--- Actual Exam Sunday/02. Cat Shirt.py
+++ Actual Exam Sunday/02. Cat Shirt.py
@@ -11,7 +11,7 @@ Denim = 20
 Twill = 16
 Flannel = 11
 
-cost_in_meters = (sleeve_size * 2 + front_size * 2) * 1.1 / 100
+cost_in_meters = (sleeve_size + front_size) * 2 * 1.1 / 100
 price = cost_in_meters * globals()[fabric] + 10
 if tie == 'Yes':
     price *= 1.2
